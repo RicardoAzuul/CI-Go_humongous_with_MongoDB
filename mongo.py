542f12d0acf388c4049e036e99b5edabf21cc357
Richard Blaauw <r.t.blaauw@live.com>
2021-09-13 19:28:38 +0000

    Add multiple new documents.

--- mongo.py
+++ mongo.py
@@ -23,9 +23,29 @@ connection = mongo_connect(MONGO_URI)
 
 collection = connection[DATABASE][COLLECTION]
 
-new_document = {"first": "douglas", "last": "adams", "dob": "11/03/1952", "gender": "m", "hair_color": "grey", "occupation": "writer", "nationality": "british"}
+# new_document = {"first": "douglas", "last": "adams", "dob": "11/03/1952", "gender": "m", "hair_color": "grey", "occupation": "writer", "nationality": "british"}
 
-collection.insert(new_document)
+# collection.insert(new_document)
+
+new_documents = [{
+    "first": "terry",
+    "last": "pratchett",
+    "dob": "28/04/1948",
+    "gender": "m",
+    "hair_color": "not much",
+    "occupation": "writer",
+    "nationality": "british"
+}, {
+    "first": "george",
+    "last": "rr martin",
+    "dob": "20/09/1948",
+    "gender": "m",
+    "hair_color": "white",
+    "occupation": "writer",
+    "nationality": "american"
+}]
+
+collection.insert_many(new_documents)
 
 documents = collection.find()
 
